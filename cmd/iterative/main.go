734c488c4486beb49871712c50e397145ddfbc85
Colin <china_men@protonmail.com>
2020-12-14 20:07:35 +0100

    Adding a main for iterative implementation as a canonical example

---
+++ cmd/iterative/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"github.com/clnbs/karateChop/internal/app/binarychop"
+)
+
+func main() {
+	// Canonical example to use recursive binary chop
+	tab := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	chop := binarychop.NewBinaryChop(&binarychop.Iterative{})
+	valueToFind := 8
+	index := chop.Execute(valueToFind, tab)
+	if index == -1 {
+		fmt.Println("no value found")
+	}
+	fmt.Println("value", valueToFind, "found at index", index)
+}
