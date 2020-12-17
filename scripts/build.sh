2deecd1cbcb8fe044185b2916200289b38bd01e4
Colin <china_men@protonmail.com>
2020-12-17 12:07:32 +0100

    Updating build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -46,50 +46,6 @@ build_from_docker() {
   check_command_success $RESULT 0 "Could not remove builder container"
 }
 
-build_recursive() {
-  green echo "Start building binary chop recursive container"
-  docker build -t recursive:build . -f ./build/package/recursive/Dockerfile
-  RESULT=$?
-  check_command_success $RESULT 0 "Could not build binary chop recursive container"
-
-  green echo "Creating container to build binary chop recursive"
-  docker container create --name extract recursive:build
-  RESULT=$?
-  check_command_success $RESULT 0 "Could not start builder container"
-
-  green echo "Extracting binary from builder container"
-  docker container cp extract:/go/src/github.com/clnbs/karateChop/recursive.bin ./recursive.bin
-  RESULT=$?
-  check_command_success $RESULT 0 "Could not extract binary from builder image"
-
-  green echo "Deleting builder container"
-  docker container rm -f extract
-  RESULT=$?
-  check_command_success $RESULT 0 "Could not remove builder container"
-}
-
-build_iterative() {
-  green echo "Start building binary chop iterative container"
-  docker build -t iterative:build . -f ./build/package/iterative/Dockerfile
-  RESULT=$?
-  check_command_success $RESULT 0 "Could not build binary chop recursive container"
-
-  green echo "Creating container to build binary chop recursive"
-  docker container create --name extract iterative:build
-  RESULT=$?
-  check_command_success $RESULT 0 "Could not start builder container"
-
-  green echo "Extracting binary from builder container"
-  docker container cp extract:/go/src/github.com/clnbs/karateChop/iterative.bin ./iterative.bin
-  RESULT=$?
-  check_command_success $RESULT 0 "Could not extract binary from builder image"
-
-  green echo "Deleting builder container"
-  docker container rm -f extract
-  RESULT=$?
-  check_command_success $RESULT 0 "Could not remove builder container"
-}
-
 OPTION=$1
 
 if [ -z "$OPTION"  ]; then
